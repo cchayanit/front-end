085d4497cdae0b2ef44cdfde02d04a21dda72fbf
cchayanit <chayanit.pm@gmail.com>
2021-04-22 11:10:55 +0700

    separate component(header,home)

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    LogInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
